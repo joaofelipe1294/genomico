e3594237d90484cbd7fa61c8cc8c645b0cde34f0
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-06-03 10:39:03 -0300

    Adicionadas validações do tipo presence nos atributos name, login, password e user_kind.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   belongs_to :user_kind
   has_secure_password
   validates :login, uniqueness: true
+  validates :login, :password, :name, :user_kind_id, presence: true
 end
