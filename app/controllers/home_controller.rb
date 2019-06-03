68c4ef60b3a249dc716f3cdbcba4354a266f4128
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-06-03 11:58:04 -0300

    Adicionado login de usuário, no momento sem validar se o usuário é admin ou usuário comum.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,32 @@ class HomeController < ApplicationController
   end
 
   def login
-  	puts 'ESTOU NO LOGIN'
+  	puts 'ESTOU NO LOGIN !!!'
+  	puts user_params
+  	user = User.find_by({login: user_params[:login]})
+  	if user
+	  	if user.authenticate user_params[:password]
+	  		session[:user_id] = user.id
+	  		redirect_to users_path
+			else
+				flash[:error] = 'Login ou senha inválidos.'
+				redirect_to root_path
+			end
+		else
+			flash[:error] = 'Login ou senha inválidos.'
+			redirect_to root_path
+		end
   end
+
+
+
+  private 
+
+  def user_params
+  	params.permit(:login, :password)
+  end
+
+
+
+
 end
