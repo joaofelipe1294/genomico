8627c1eb59deb39e4ffbda65721b24274aa882d6
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-06-04 15:37:53 -0300

    Concluída edição de usuário.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,14 +44,12 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update user_params
+      flash[:success] = 'Usuário editado com sucesso.'
+      redirect_to home_admin_index_path
+    else
+      @user_kinds = UserKind.all.order(name: :desc)
+      render :edit
     end
   end
 
@@ -80,7 +78,7 @@ class UsersController < ApplicationController
       render :index
     end
   end
-  
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
