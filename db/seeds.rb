36e9b9f149602a71be7086224c8af97c5d306a56
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-06-05 09:10:22 -0300

    Adicionados valores default de Field ao arquivo seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,11 @@ User.create({
 	name: 'root',
 	user_kind: UserKind.find_by({name: 'admin'})
 })
+
+Field.create([
+	{name: 'Citogenética'},
+	{name: 'FISH'},
+	{name: 'Biologia Molecular'},
+	{name: 'Anatomia Patológica'},
+	{name: 'Imunofenotipagem'},
+])
