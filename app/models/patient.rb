b9ddb8754ab3a8df22231185a67c6bcc0ccb1407
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-06-07 09:46:46 -0300

    Adicionada validação de paciente por meio de nome, nome_da_mae e data_de_nascimento.

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,5 @@
 class Patient < ActiveRecord::Base
 	validates :medical_record, uniqueness: true	
 	validates :name, :birth_date, :mother_name, :medical_record, presence: true 
+	validates :name, uniqueness: {scope: [:birth_date, :mother_name], allow_blank: false}
 end
