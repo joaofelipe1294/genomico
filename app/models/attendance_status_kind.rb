6001966300208c3952e12097735c5f35db2a1a6c
joao_felipe <joaofelipe1294@gmail.com>
2019-06-09 13:28:57 -0300

    Adicionado modelo AttendanceStatusKinds.

---
+++ app/models/attendance_status_kind.rb
@@ -0,0 +1,2 @@
+class AttendanceStatusKind < ActiveRecord::Base
+end
---
+++ test/models/attendance_status_kind_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AttendanceStatusKindTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
