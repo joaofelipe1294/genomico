a066143f118685331aef0ae113c388fcf2d7e683
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-06-11 09:20:36 -0300

    Adicionada rota customizada para cadastro de novo atendimento.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :attendances
-  get 'home_user/index'
   root 'home#index'
+  get 'home_user/index'
   post 'home/longin', to: 'home#login'
   post 'home/logout', to: 'home#logout'
   get 'home_admin/index', to: 'home_admin#index'
@@ -9,7 +8,9 @@ Rails.application.routes.draw do
   post 'users/:id/change_password', to: 'users#change_password', as: :change_password
   get 'users/:id/change_password', to: 'users#change_password_view', as: :change_password_view
   post 'offered_exams/:id/activate', to: 'offered_exams#active_exam', as: :activate_offered_exam
+  get 'attendances/new/patient/:id', to: 'attendances#new', as: :new_attendance
   resources :users
+  resources :attendances, except: [:new]
   resources :offered_exams
   resources :patients, except: [:delete]
 end
