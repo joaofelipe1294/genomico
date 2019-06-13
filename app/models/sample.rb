2e8898c862ab9cd64a3c77169b033f31c97946b8
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-06-13 15:43:04 -0300

    Adicionada geração do rótulo de referencia de uma amostra.

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -2,10 +2,19 @@ class Sample < ActiveRecord::Base
   belongs_to :sample_kind
   belongs_to :attendance
   after_initialize :default_values
+  before_save :set_refference_label
 
   def default_values
 		self.has_sub_sample = false if self.has_sub_sample.nil?
 		self.entry_date = Date.today if self.entry_date.nil?
+	end
+
+	private 
+
+	def set_refference_label
+		sample_kind.refference_index += 1
+		sample_kind.save
+		self.refference_label = "#{Date.today.year}-#{sample_kind.acronym}-#{sample_kind.refference_index}"
 	end
 
 end
