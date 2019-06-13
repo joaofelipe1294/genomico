73d233a10094445516b7892165fca3c048351415
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-06-13 14:09:31 -0300

    Adicioandos valores default de Sample.

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,4 +1,11 @@
 class Sample < ActiveRecord::Base
   belongs_to :sample_kind
   belongs_to :attendance
+  after_initialize :default_values
+
+  def default_values
+		self.has_sub_sample = false if self.has_sub_sample.nil?
+		self.entry_date = Date.today if self.entry_date.nil?
+	end
+
 end
