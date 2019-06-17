65553964c80eb381cc4e82a132f01ed88f4ba945
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-06-17 09:31:30 -0300

    Concluído cadastro de amostra na página de workflow.

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -1,4 +1,19 @@
 class SamplesController < ApplicationController
+  
   def create
+  	@sample = Sample.new sample_params
+  	if @sample.save
+  		flash[:success] = 'Amostra cadastrada com sucesso.'
+  		redirect_to workflow_path(@sample.attendance)
+		else
+			render workflow_path(@sample.attendance)
+		end
   end
+
+  private
+
+  	def sample_params
+			params.require(:sample).permit(:sample_kind_id, :collection_date, :bottles_number, :storage_location, :attendance_id)
+  	end
+
 end
