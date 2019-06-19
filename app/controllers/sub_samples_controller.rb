b345ea7505e3769b5d488465556fa30db5c0057b
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-06-19 11:28:16 -0300

    Adicionada variável de instância referente a @sub_sample utilizada no form de cadastro.

--- app/controllers/sub_samples_controller.rb
+++ app/controllers/sub_samples_controller.rb
@@ -1,7 +1,15 @@
 class SubSamplesController < ApplicationController
+  
+  #GET /sub_samples/sample/:id/new
+  def new
+  	@sub_sample = SubSample.new({sample: Sample.find(params[:id])})
+  end
+
+  def create
+	end
+
   def edit
   end
 
-  def new
-  end
+  
 end
