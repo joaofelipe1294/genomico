9661039a00f28843e1d7891e4cb1b1bf7b4b9132
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-06-19 13:55:56 -0300

    Adicionada exibição do rótulo da amostra que será extraída.

--- app/controllers/sub_samples_controller.rb
+++ app/controllers/sub_samples_controller.rb
@@ -5,12 +5,14 @@ class SubSamplesController < ApplicationController
   	sample = Sample.find params[:id]
   	@sub_sample_kinds = SubSampleKind.all.order :name
   	if sample.sample_kind == SampleKind.find_by({name: 'Swab bucal.'})
-  		@sub_sample_kinds -= SubSampleKind.find_by('Pellet de FISH') 
+  		@sub_sample_kinds = @sub_sample_kinds - [SubSampleKind.find_by({name: 'Pellet de FISH'})]
 		end
   	@sub_sample = SubSample.new({sample: sample})
+  	@processing_equipments = ProcessingEquipment.all.order :name
   end
 
   def create
+
 	end
 
   def edit
