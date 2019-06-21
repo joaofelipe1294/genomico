c9ab05beea8c1932641a228d34b0917827c48851
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-06-21 09:59:42 -0300

    Adicionada geração dos valore default utilizados pelo modelo subsample.

--- app/models/subsample.rb
+++ app/models/subsample.rb
@@ -4,4 +4,17 @@ class Subsample < ActiveRecord::Base
   has_one :nanodrop_report
   has_one :qubit_report
   accepts_nested_attributes_for :nanodrop_report, :qubit_report
+  before_save :add_default_values
+
+  private
+
+  def add_default_values
+		subsample_kind.refference_index += 1
+		subsample_kind.save
+		self.refference_label = "#{Date.today.year}-#{subsample_kind.acronym}-#{subsample_kind.refference_index}"
+		self.collection_date = DateTime.now
+		self.sample.update({has_subsample: true})
+  end
+
+
 end
