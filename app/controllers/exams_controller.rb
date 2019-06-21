d480e8183dd6a55fc9e4d131055776b8e0baecd3
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-06-21 14:56:21 -0300

    Adicionada variável global com as possibilidades de amostra a serem utilizadas em um exame.

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -2,6 +2,12 @@ class ExamsController < ApplicationController
   
 	def start
 		@exam = Exam.find params[:id]
+		@samples = []
+		samples = @exam.attendance.samples
+		samples.each do |sample|
+			@samples += sample.subsamples if sample.has_subsample
+		end
+		@samples += samples
 	end
 
   def new
