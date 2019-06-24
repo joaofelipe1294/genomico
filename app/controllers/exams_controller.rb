3119fb1a01a8f681781f8cf4a023257813609ef0
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-06-24 11:06:59 -0300

    Adicionado método que instancia o objeto do tipo exam na controller exams.

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -1,4 +1,6 @@
 class ExamsController < ApplicationController
+  before_action :set_exam, only: [:initiate, :tecnical_released, :in_repeat]
+
   
 	def start
 		@exam = Exam.find params[:id]
@@ -11,46 +13,43 @@ class ExamsController < ApplicationController
 	end
 
 	def initiate
-		exam = Exam.find params[:id]
-		exam.exam_status_kind = ExamStatusKind.find_by({name: 'Em andamento'})
+		@exam.exam_status_kind = ExamStatusKind.find_by({name: 'Em andamento'})
 		sample = Sample.find_by({refference_label: exam_params[:refference_label]})
 		if sample.nil? == false
-			exam.sample = sample
-			exam.uses_subsample = false
+			@exam.sample = sample
+			@exam.uses_subsample = false
 		else
-			exam.subsample = Subsample.find_by({refference_label: exam_params[:refference_label]})
-			exam.uses_subsample = true
+			@exam.subsample = Subsample.find_by({refference_label: exam_params[:refference_label]})
+			@exam.uses_subsample = true
 		end
-		if exam.save
+		if @exam.save
 			flash[:success] = 'Exame iniciado.'
-			redirect_to workflow_path(exam.attendance)
+			redirect_to workflow_path(@exam.attendance)
 		else
 			flash[:warning] = 'Erro ao iniciar exame, tente novamente mais tarde.'
-			redirect_to workflow_path(exam.attendance)
+			redirect_to workflow_path(@exam.attendance)
 		end
 	end
 
 	def tecnical_released
-		exam = Exam.find params[:id]
-		exam.exam_status_kind = ExamStatusKind.find_by({name: 'Liberado técnico'})
-		if exam.save
+		@exam.exam_status_kind = ExamStatusKind.find_by({name: 'Liberado técnico'})
+		if @exam.save
 			flash[:success] = 'Status de exame alterado para liberado técnico.'
-			redirect_to workflow_path(exam.attendance)
+			redirect_to workflow_path(@exam.attendance)
 		else
 			flash[:warning] = 'Erro ao alterar status de exame, tente novamente mais tarde.'
-			redirect_to workflow_path(exam.attendance)
+			redirect_to workflow_path(@exam.attendance)
 		end
 	end
 
 	def in_repeat
-		exam = Exam.find params[:id]
-		exam.exam_status_kind = ExamStatusKind.find_by({name: 'Em repetição'})
-		if exam.save
+		@exam.exam_status_kind = ExamStatusKind.find_by({name: 'Em repetição'})
+		if @exam.save
 			flash[:success] = 'Status de exame alterado para em repetição.'
-			redirect_to workflow_path(exam.attendance)
+			redirect_to workflow_path(@exam.attendance)
 		else
 			flash[:warning] = 'Erro ao alterar status de exame, tente novamente mais tarde.'
-			redirect_to workflow_path(exam.attendance)
+			redirect_to workflow_path(@exam.attendance)
 		end
 	end
 
@@ -60,7 +59,9 @@ class ExamsController < ApplicationController
 			params.require(:exam).permit(:offered_exam_id, :refference_label)
   	end
 
-	# TODO adicionar método que retorna o exame que será trabalahdo no método
+  	def set_exam
+  		@exam = Exam.find params[:id]
+		end 
 
 	# TODO adicionar método que exibe mensagem e redireciona após execução do método
 
