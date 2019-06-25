fd94be3c0b55530707469de812a884122932c4bc
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-06-25 13:22:49 -0300

    Adicionados relacionamentos entre os modelos patent e hospital.

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,5 +1,6 @@
 class Hospital < ActiveRecord::Base
-  belongs_to :patient
+  # belongs_to :patient
   validates :name, presence: true
   validates :name, uniqueness: true
+  has_many :patients 
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,10 +1,8 @@
 class Patient < ActiveRecord::Base
 	validates :medical_record, uniqueness: true	
-	validates :name, :birth_date, :mother_name, :medical_record, presence: true 
+	validates :name, :birth_date, :mother_name, :medical_record, :hospital, presence: true 
 	validates :name, uniqueness: {scope: [:birth_date, :mother_name], allow_blank: false}
 	paginates_per 10
 	has_many :attendances
-
-	# TODO adicionar relacionamento com o hospital que enviou a amostra.
-
+	belongs_to :hospital
 end
