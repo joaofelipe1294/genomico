c518995ba5c59fde1d6a4e2612ff7c8387215d35
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-06-27 14:11:56 -0300

    Adicionada geração da referência dos atendimentos aos mapas de trabalho.

--- app/models/work_map.rb
+++ app/models/work_map.rb
@@ -5,5 +5,19 @@ class WorkMap < ActiveRecord::Base
   has_and_belongs_to_many :subsamples
   has_and_belongs_to_many :attendances
   attr_accessor :samples_ids
+  after_save :set_attendances
+
+  private
+
+  	def set_attendances
+  		attendance_ids = []
+  		samples.each do |sample|
+  			attendance_ids.push(sample.attendance.id)
+			end
+			subsamples.each do |subsample|
+				attendance_ids.push(subsample.attendance.id)
+			end 
+			self.attendance_ids = attendance_ids
+		end
 
 end
