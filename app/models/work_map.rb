8a546608d4ddde9871ca9e0f3a76d2656f30d727
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-06-28 10:57:31 -0300

    Corrigido bug que estava gerando referencias duplicadas entre work_maps e atendimentos.

--- app/models/work_map.rb
+++ app/models/work_map.rb
@@ -1,13 +1,13 @@
 class WorkMap < ActiveRecord::Base
-	has_attached_file :map
-  validates_attachment_content_type :map, :content_type => ["application/pdf"]
+	attr_accessor :samples_ids
+  before_save :set_attendances
+  has_attached_file :map
   has_and_belongs_to_many :samples
   has_and_belongs_to_many :subsamples
   has_and_belongs_to_many :attendances
-  attr_accessor :samples_ids
-  before_save :set_attendances
   validates :name, uniqueness: true
   validates :name, :map, presence: true
+  validates_attachment_content_type :map, :content_type => ["application/pdf"]
 
   private
 
@@ -20,7 +20,7 @@ class WorkMap < ActiveRecord::Base
 			subsamples.each do |subsample|
 				attendance_ids.push(subsample.attendance.id)
 			end 
-			self.attendance_ids = attendance_ids
+			self.attendance_ids = attendance_ids.uniq
 		end
 
 end
