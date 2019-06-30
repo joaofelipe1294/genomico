3b8790efe51e92b36d35c863b689d3c7d17de205
joao_felipe <joaofelipe1294@gmail.com>
2019-06-30 12:18:43 -0300

    Adicionada factory de Field e adiciondos testes que validam as validações do modelo field.

--- spec/factories/fields.rb
+++ spec/factories/fields.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :field do
-    
+    name { Faker::Book.genre }
   end
 end
--- spec/models/field_spec.rb
+++ spec/models/field_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Field, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Field validations' do
+  	
+  	it 'is expected to create a new field.' do
+  		field = create(:field)
+  		expect(field).to be_valid
+		end
+
+		it 'is expected to fail on try to create a field with empty name' do
+			field = build(:field, name: nil)
+			field.save
+			expect(field).to be_invalid
+		end 
+
+		it 'is expected to fail on try to create two fields with same name' do
+			field = build(:field)
+			field.save
+			second_field = build(:field, name: field.name)
+			second_field.save
+			expect(second_field).to be_invalid
+			expect(field).to be_valid
+		end
+
+	end 
 end
