254f8fb851fefcc7ac2dc2d66c98fcf190b16404
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-07-01 16:23:58 -0300

    Adicionado método executado antes de criar um sample_kind, gerando refference_index com o valor 0.

--- app/models/sample_kind.rb
+++ app/models/sample_kind.rb
@@ -2,4 +2,12 @@ class SampleKind < ActiveRecord::Base
 	validates :name, :acronym, uniqueness: true
 	validates :name, :acronym, presence: true
 	has_many :samples
+	before_create :set_refference_index
+
+	private
+
+		def set_refference_index
+			self.refference_index = 0 if self.refference_index.nil?
+		end
+
 end
