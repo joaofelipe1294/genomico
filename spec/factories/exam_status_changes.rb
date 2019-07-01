d0e4b09467bea900f52e62dd6853e0415048b3f8
joao_felipe <joaofelipe1294@gmail.com>
2019-06-30 21:28:07 -0300

    Removidos specs gerados e nao utilizados no momento.

--- spec/factories/exam_status_changes.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :exam_status_change do
-    exam { nil }
-    exam_status_kind { nil }
-    change_date { "2019-06-28 13:38:36" }
-  end
-end
--- spec/factories/work_maps.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :work_map do
-    data { "2019-06-26 12:59:47" }
-  end
-end
