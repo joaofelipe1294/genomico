f294120c9010451a46142f4d33a0f332ff30ee7a
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-07-01 11:34:23 -0300

    Alterada cláusula unique de medical_record, agora ele está relacionado a hospital.

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,5 @@
 class Patient < ActiveRecord::Base
-	validates :medical_record, uniqueness: true	
+	validates :medical_record, uniqueness: {scope: [:hospital], allow_blank: false}
 	validates :name, :birth_date, :mother_name, :medical_record, :hospital, presence: true 
 	validates :name, uniqueness: {scope: [:birth_date, :mother_name, :hospital], allow_blank: false}
 	paginates_per 10
