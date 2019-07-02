0456d79a9c123a853a9a9339c795efe829368438
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-07-02 11:10:35 -0300

    Adicionada factory que gera uma attendance para os testes rspec.

--- spec/factories/attendances.rb
+++ spec/factories/attendances.rb
@@ -1,6 +1,18 @@
 FactoryBot.define do
   factory :attendance do
-    lis_code { Faker::Number.number(9).to_s }
-    patient { create(:patient) }
+  	desease_stage { create(:desease_stage) }
+  	cid_code { Faker::Number.number(5).to_s }
+  	lis_code { Faker::Number.number(8).to_s }
+  	start_date { nil }
+  	finish_date { nil }
+  	patient { create(:patient) }
+  	attendance_status_kind { create(:attendance_status_kind) }
+  	doctor_name { Faker::Name.name }
+  	doctor_crm { Faker::Number.number(5).to_s }
+  	observations { Faker::Lorem.sentence }
+  	health_ensurance { create(:health_ensurance) }
+  	report { nil }
+  	samples_attributes { [attributes_for(:sample)] }
+  	exams_attributes { [attributes_for(:exam), attributes_for(:exam) ]}
   end
 end
