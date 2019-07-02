3714ba8600edf7df76943001526806813840847e
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-07-02 09:31:51 -0300

    Alterado dado Fake usado em SampleKind.name.

--- spec/factories/sample_kinds.rb
+++ spec/factories/sample_kinds.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :sample_kind do
-    name {Faker::Hacker.verb}
+    name { Faker::Company.name }
     acronym { Faker::Hacker.abbreviation }
     refference_index {Faker::Number.number(3)}
   end
