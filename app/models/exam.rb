fee163dffeb0e08c4b5bb7bede31bf60784b155a
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-07-02 14:15:20 -0300

    Adicionados testes que validam o modelo Exam.

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -1,23 +1,19 @@
 class Exam < ActiveRecord::Base
   attr_accessor :refference_label
+  validates :exam_status_kind, presence: true
   belongs_to :offered_exam
   belongs_to :exam_status_kind
   belongs_to :attendance
   belongs_to :sample
   belongs_to :subsample
   has_many :exam_status_changes
-  after_initialize :default_values
-  before_save :set_start_date
-  
+  before_validation :default_values
 
   private
 
   def default_values
   	self.exam_status_kind = ExamStatusKind.find_by({name: 'Aguardando início'}) if self.exam_status_kind.nil?
-  end
-
-  def set_start_date
-    self.start_date = DateTime.now
+    self.start_date = DateTime.now if self.start_date.nil?
   end
 
 end
--- spec/models/exam_spec.rb
+++ spec/models/exam_spec.rb
@@ -7,8 +7,45 @@ RSpec.describe Exam, type: :model do
 		it 'without attendance' do
 			exam = build(:exam)
 			exam.save
-			expect(exam).to be_invalid
+			expect(exam).to be_valid
 		end
+
+		it 'without exam_status_kind' do
+			exam_status_kind = create(:exam_status_kind, name: 'Aguardando início')
+			exam = build(:exam, exam_status_kind: nil)
+			exam.save
+			expect(exam).to be_valid
+			expect(exam.exam_status_kind).to eq(exam_status_kind)
+		end
+
+		it 'without finish_date' do
+			exam = build(:exam, finish_date: nil)
+			exam.save
+			expect(exam).to be_valid
+		end
+
+		it 'without start_date' do
+			exam = build(:exam, start_date: nil)
+			exam.save
+			expect(exam).to be_valid
+			expect(exam.start_date).to eq(Date.today)
+		end
+
+	end
+
+	context 'Relations' do
+
+		it { should belong_to(:attendance) }
+
+		it { should belong_to(:sample) }
+
+		it { should belong_to(:subsample) }
+
+		it { should belong_to(:exam_status_kind) }
+
+		it { should belong_to(:offered_exam) }
+
+		it { should have_many(:exam_status_changes) }
 
 	end
 
