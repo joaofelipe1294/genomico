634e97ab469bfca643227e7111be982682c8f5eb
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-07-02 16:06:38 -0300

    Adicionados testes do modelo work_map.

---
+++ spec/factories/work_maps.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :work_map do
+    name { Faker::Name.name }
+    date { Faker::Date.between(2.days.ago, Date.today) }
+    map { File.new(Rails.root + 'spec/support_files/PDF.pdf') }
+    attendances { [create(:attendance), create(:attendance)] }
+  end
+end
---
+++ spec/models/work_map_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe WorkMap, type: :model do
+
+	context 'Validations' do
+
+		it 'correct' do
+			work_map = create(:work_map)
+			expect(work_map).to be_valid
+		end
+
+		it 'without name' do
+			work_map = build(:work_map, name: nil)
+			work_map.save
+			expect(work_map).to be_invalid
+		end
+
+		it 'duplicated name' do
+			work_map = create(:work_map)
+			duplicated = build(:work_map, name: work_map.name)
+			duplicated.save
+			expect(duplicated).to be_invalid
+		end
+
+		it 'without map' do
+			work_map = build(:work_map, map: nil)
+			work_map.save
+			expect(work_map).to be_invalid
+		end
+
+	end 
+
+	context 'Relations' do
+
+		it { should have_and_belong_to_many(:attendances) }
+
+		it { should have_and_belong_to_many(:subsamples) }
+
+		it { should have_and_belong_to_many(:samples) }
+ 
+	end
+
+end
