457aaa79bdd0aa9056df43f8adf938e16cfca978
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-07-02 11:12:22 -0300

    Alterado tipo do dados Fake utilizado na factory de SampleKind.acronym.

--- spec/factories/sample_kinds.rb
+++ spec/factories/sample_kinds.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :sample_kind do
     name { Faker::Company.name }
-    acronym { Faker::Hacker.abbreviation }
+    acronym { Faker::Hipster.word }
     refference_index {Faker::Number.number(3)}
   end
 end
