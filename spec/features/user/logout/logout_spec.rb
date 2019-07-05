e4f876b141c6f2d32d95509ba8d28149d0b38e1a
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-07-05 11:00:52 -0300

    Extraído código da função que fza o login de um usuário para o arquivo helpers/user.

--- spec/features/user/logout/logout_spec.rb
+++ spec/features/user/logout/logout_spec.rb
@@ -1,20 +1,10 @@
 require 'rails_helper'
+require 'helpers/user'
 
 RSpec.feature "User::Logout::Logouts", type: :feature do
 
 	it 'logout', js: false do
-		user_kind = UserKind.create({name: 'user'})
-		user = User.create({
-			login: Faker::Internet.username,
-			name: Faker::Name.name, 
-			password: '1234',
-			user_kind: user_kind
-		})
-		visit(root_path)
-		fill_in('login', with: user.login)
-		fill_in('password', with: user.password)
-		click_button(class: 'btn')
-		expect(page).to have_current_path home_user_index_path
+		user_do_login
 		click_link(id: 'logout-link')
 		expect(page).to have_current_path root_path
 	end
---
+++ spec/helpers/user.rb
@@ -0,0 +1,14 @@
+def user_do_login
+	user_kind = UserKind.create({name: 'user'})
+		user = User.create({
+			login: Faker::Internet.username,
+			name: Faker::Name.name, 
+			password: '1234',
+			user_kind: user_kind
+		})
+		visit(root_path)
+		fill_in('login', with: user.login)
+		fill_in('password', with: user.password)
+		click_button(class: 'btn')
+		expect(page).to have_current_path home_user_index_path
+end
