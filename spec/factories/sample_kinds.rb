b7665153830f0501e3da83bd099600d1d475db0a
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-07-09 15:10:02 -0300

    Alterados tipos de dados Fake utilizados nas factories de work_map e sample_kind

--- spec/factories/sample_kinds.rb
+++ spec/factories/sample_kinds.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :sample_kind do
     name { Faker::Company.name }
-    acronym { Faker::Hipster.word }
+    acronym { Faker::Company.name }
     refference_index {Faker::Number.number(3)}
   end
 end
--- spec/factories/work_maps.rb
+++ spec/factories/work_maps.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :work_map do
-    name { Faker::Name.name }
+    name { Faker::Company.name }
     date { Faker::Date.between(2.days.ago, Date.today) }
     map { File.new(Rails.root + 'spec/support_files/PDF.pdf') }
     attendances { [create(:attendance), create(:attendance)] }
