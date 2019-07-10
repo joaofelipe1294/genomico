c8fdf8903d28d99f326e44d99791764bf7b40694
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-07-10 14:09:13 -0300

    Adicionado método que faz o login de um usuário já cadastrado pelo arquivo seed.

--- spec/helpers/user.rb
+++ spec/helpers/user.rb
@@ -2,7 +2,7 @@ def user_do_login
 	user_kind = UserKind.create({name: 'user'})
 		user = User.create({
 			login: Faker::Internet.username,
-			name: Faker::Name.name, 
+			name: Faker::Name.name,
 			password: '1234',
 			user_kind: user_kind
 		})
@@ -11,4 +11,13 @@ def user_do_login
 		fill_in('password', with: user.password)
 		click_button(class: 'btn')
 		expect(page).to have_current_path home_user_index_path
+end
+
+
+def do_login
+	visit(root_path)
+	fill_in('login', with: 'user')
+	fill_in('password', with: '1234')
+	click_button(class: 'btn')
+	expect(page).to have_current_path home_user_index_path
 end
