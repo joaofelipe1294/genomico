ac7c7956b48e8fa6dec99e9b85fe7b145c42232a
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-07-12 16:43:13 -0300

    Adicionada configuração para exibir arquivos estáticos.

---
+++ config/schedule.rb
@@ -0,0 +1,7 @@
+# every :day, at: '1:00 am' do
+#   command "backup perform --trigger app_genomico_backup"
+# end
+
+every 1.minute do
+  # command "backup perform --trigger app_genomico_backup"
+end
