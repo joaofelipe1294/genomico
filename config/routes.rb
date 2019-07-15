71373c21bd9f11f26921fd279a90d921bb5ec10f
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-07-15 15:28:29 -0300

    Adicionada controller que gerencia os backups.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
+  get 'backups/index'
   get 'panels/exams'
-
   get 'panels/attendances'
-
   resources :work_maps
   resources :hospitals
   root 'home#index'
@@ -15,7 +14,7 @@ Rails.application.routes.draw do
   post 'users/:id/change_password', to: 'users#change_password', as: :change_password
   get 'users/:id/change_password', to: 'users#change_password_view', as: :change_password_view
 
-  get 'exams/:id/edit', to: 'exams#edit', as: :edit_exam 
+  get 'exams/:id/edit', to: 'exams#edit', as: :edit_exam
   get 'exams/:id/start', to: 'exams#start', as: :start_exam
   patch 'exams/:id', to: 'exams#update', as: :update_exam
   patch 'exams/:id/initiate', to: 'exams#initiate', as: :initiate_exam
@@ -25,7 +24,7 @@ Rails.application.routes.draw do
 
   get 'samples/new/attendance/:id', to: 'samples#new', as: :new_sample
   get 'subsamples/sample/:id/new', to: 'subsamples#new', as: :new_sub_sample
-  
+
   get 'attendances/:id/workflow', to: 'attendances#workflow', as: :workflow
   get 'attendances/new/patient/:id', to: 'attendances#new', as: :new_attendance
   get 'attendances/lis_code', to: 'attendances#find_by_lis_code', as: :lis_search
@@ -35,7 +34,7 @@ Rails.application.routes.draw do
   post 'offered_exams/:id/activate', to: 'offered_exams#active_exam', as: :acitvate_offered_exam
 
   get 'patient/:id/attendances', to: 'attendances#attendances_from_patient', as: :attendances_from_patient
-  
+
   resources :subsamples
   resources :samples, except: [:new]
   resources :users
