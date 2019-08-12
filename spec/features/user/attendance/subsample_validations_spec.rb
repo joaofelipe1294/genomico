4cb8e931c87036e531ed17a0c167767ae000a475
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-08-12 12:16:39 +0000

    Removida pasta com as partials que não são mais utilizadas no cadastro de atendimento.

--- spec/features/user/attendance/subsample_validations_spec.rb
+++ spec/features/user/attendance/subsample_validations_spec.rb
@@ -2,6 +2,19 @@ require 'rails_helper'
 require 'helpers/attendance'
 require 'helpers/user'
 
+def extract_subsample
+  click_button id: 'sample_nav'
+  click_link class: 'new-subsample', match: :first
+  select(SubsampleKind.all.sample.name, from: 'subsample[subsample_kind_id]').select_option
+  fill_in 'subsample[storage_location]', with: 'F -80'
+  fill_in 'subsample[nanodrop_report_attributes][rate_260_280]', with: Faker::Number.decimal(l_digits: 2)
+  fill_in 'subsample[nanodrop_report_attributes][rate_260_230]', with: Faker::Number.decimal(l_digits: 2)
+  fill_in 'subsample[nanodrop_report_attributes][concentration]', with: Faker::Number.decimal(l_digits: 2)
+  fill_in 'subsample[qubit_report_attributes][concentration]', with: Faker::Number.decimal(l_digits: 2)
+  click_button id: 'btn-save-subsample'
+  click_button id: 'subsample_nav'
+end
+
 
 RSpec.feature "User::Attendance::SubsampleValidations", type: :feature do
 
@@ -9,19 +22,18 @@ RSpec.feature "User::Attendance::SubsampleValidations", type: :feature do
     create_attendance
     do_login
     navigate_to_workflow
+    extract_subsample
   end
 
-  it "navigate to subsamples tab", js: true do
-    click_button id: 'sample_nav'
-    click_link class: 'new-subsample', match: :first
-    select(SubsampleKind.all.sample.name, from: 'subsample[subsample_kind_id]').select_option
-    fill_in 'subsample[storage_location]', with: 'F -80'
-    fill_in 'subsample[nanodrop_report_attributes][rate_260_280]', with: Faker::Number.decimal(l_digits: 2)
-    fill_in 'subsample[nanodrop_report_attributes][rate_260_230]', with: Faker::Number.decimal(l_digits: 2)
-    fill_in 'subsample[nanodrop_report_attributes][concentration]', with: Faker::Number.decimal(l_digits: 2)
-    fill_in 'subsample[qubit_report_attributes][concentration]', with: Faker::Number.decimal(l_digits: 2)
-    click_button id: 'btn-save-subsample'
+  # it "navigate to subsamples tab", js: true do
+  #   expect(find(id: 'success-warning').text).to eq "Subamostra cadastrada com sucesso."
+  # end
+
+  it "edit subsample", js: true do
     click_button id: 'subsample_nav'
+    sleep 8
   end
+
+
 
 end
