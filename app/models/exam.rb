940d6b2dc26baaf54506be13f4dee7307a72a159
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-08-13 20:24:59 +0000

    Adicionado indicador que exibe os exames em andamento por área.

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -9,6 +9,19 @@ class Exam < ActiveRecord::Base
   has_many :exam_status_changes
   before_validation :default_values
 
+  def self.in_progress_by_field field
+    conn = ActiveRecord::Base.connection
+    result = conn.execute "SELECT e.id,
+                         oe.name,
+                         f.name
+                  FROM exams e
+                       INNER JOIN offered_exams oe ON oe.id = e.offered_exam_id
+                       INNER JOIN fields f ON f.id = oe.field_id
+                  WHERE e.exam_status_kind_id <> (SELECT id FROM exam_status_kinds WHERE name = 'Concluído')
+                    AND f.id = (select id from fields where name = #{conn.quote(field)});"
+    result.cmd_tuples
+  end
+
   private
 
   def default_values
