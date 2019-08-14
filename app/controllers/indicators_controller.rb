da29be2001181b6b766704b083a68a7c24ab4f34
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-08-14 16:38:18 +0000

    Adicionada possibilidade de selecionar um intervalo para buscar os exames que foram concluídos neste período.

--- app/controllers/indicators_controller.rb
+++ app/controllers/indicators_controller.rb
@@ -7,8 +7,17 @@ class IndicatorsController < ApplicationController
   end
 
   def concluded_exams
-    # Exam.where('created_at BETWEEN ? AND ?', @selected_date.beginning_of_day, @selected_date.end_of_day)
-    @concluded_exams_cont = Exam.where(exam_status_kind: ExamStatusKind.find_by({name: 'Concluído'})).size
+    if params[:start_date].nil? || params[:end_date].nil? || params[:start_date].empty? || params[:end_date].empty?
+      @start_date = 2.years.ago
+      @end_date = 1.second.ago
+      @concluded_exams_cont = Exam.where(exam_status_kind: ExamStatusKind.find_by({name: 'Concluído'})).size
+    else
+      @start_date = params[:start_date]
+      @end_date = params[:end_date]
+      @concluded_exams_cont = Exam
+                                  .where(exam_status_kind: ExamStatusKind.find_by({name: 'Concluído'}))
+                                  .where("finish_date BETWEEN ? AND ?", params[:start_date], params[:end_date]).size
+    end
   end
 
 end
