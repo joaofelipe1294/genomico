aac2c19fabd6f72a22fc40bf2f6c633751c5f0d3
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-08-14 14:28:44 +0000

    Iniciada implementação do relatório dos exames em andamento.

--- app/controllers/indicators_controller.rb
+++ app/controllers/indicators_controller.rb
@@ -5,4 +5,10 @@ class IndicatorsController < ApplicationController
   def exams_in_progress
     @exams_in_progress_count = Exam.where.not(exam_status_kind: ExamStatusKind.find_by({name: 'Concluído'})).size
   end
+
+  def concluded_exams
+    # Exam.where('created_at BETWEEN ? AND ?', @selected_date.beginning_of_day, @selected_date.end_of_day)
+    @concluded_exams_cont = Exam.where(exam_status_kind: ExamStatusKind.find_by({name: 'Concluído'})).size
+  end
+
 end
