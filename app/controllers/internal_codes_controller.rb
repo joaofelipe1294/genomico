5474969292730197bc351edbe262ece42569fb30
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-08-16 13:33:11 +0000

    Adicionada implementação inicial do método que cadastra um internal code.

--- app/controllers/internal_codes_controller.rb
+++ app/controllers/internal_codes_controller.rb
@@ -1,12 +1,23 @@
 class InternalCodesController < ApplicationController
 
-
   def new
     @fields = [Field.find_by({name: 'Imunofenotipagem'})]
     @internal_code = InternalCode.new(sample: Sample.find(params[:id]))
   end
 
+  def create
+    @internal_code = InternalCode.new internal_code_attributes
+    if @internal_code.save
+      flash[:success] = 'Código interno salvo com sucesso.'
+    else
+      flash[:error] = 'Erro ao cadastrar código interno, tente novamente mais tarde.'
+    end
+  end
 
+  private
 
+  def internal_code_attributes
+    params.require(:internal_code).permit(:sample_id, :field_id)
+  end
 
 end
--- spec/views/internal_codes/new.html.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "internal_codes/new.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
