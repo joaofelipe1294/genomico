d3a9457a5a012fa59fd32cf7a6d9c2e91d462652
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-08-20 17:10:05 +0000

    Alterada ordenação na exibição dos atendimentos, e desabilitado parâmetro observatios na exibição de um paciente

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -6,7 +6,7 @@ class AttendancesController < ApplicationController
   # GET /attendances
   # GET /attendances.json
   def index
-    @attendances = Attendance.where("doctor_name ILIKE ?", "%#{params[:doctor_name]}%").order(start_date: :asc).page params[:page]
+    @attendances = Attendance.where("doctor_name ILIKE ?", "%#{params[:doctor_name]}%").order(start_date: :desc).page params[:page]
   end
 
   # GET /attendances/1
