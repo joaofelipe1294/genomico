deb103b7ee6ca0d6bf9b3d7df78dc0975594388c
joao_felipe <joaofelipe1294@gmail.com>
2019-08-24 23:00:11 +0000

    Adicionado tratamento caso usuário logado não possua nenhuma área.

--- app/controllers/home_user_controller.rb
+++ app/controllers/home_user_controller.rb
@@ -2,19 +2,22 @@ class HomeUserController < ApplicationController
   helper_method :colors
 
   def index
-    open_exams_query = "
-        SELECT e.id
-        FROM exams e
-             INNER JOIN offered_exams oe ON oe.id = e.offered_exam_id
-        WHERE e.exam_status_kind_id <> (SELECT id FROM exam_status_kinds WHERE name = 'Concluído')
-              AND e.exam_status_kind_id <> (SELECT id FROM exam_status_kinds WHERE name = 'Aguardando início')
-              AND oe.field_id = ?;"
-    @user = User.includes(:fields).find(session[:user_id])
-    status_concluded = ExamStatusKind.find_by({name: 'Concluído'})
-    exam_ids = Exam.find_by_sql [open_exams_query, @user.fields.first.id]
-    @open_exams = Exam.where(id: exam_ids)
-    find_open_exams @user.fields.first.id
-    waiting_exams
+    @user = User.find session[:user_id]
+    unless @user.fields.empty?
+      open_exams_query = "
+          SELECT e.id
+          FROM exams e
+               INNER JOIN offered_exams oe ON oe.id = e.offered_exam_id
+          WHERE e.exam_status_kind_id <> (SELECT id FROM exam_status_kinds WHERE name = 'Concluído')
+                AND e.exam_status_kind_id <> (SELECT id FROM exam_status_kinds WHERE name = 'Aguardando início')
+                AND oe.field_id = ?;"
+      @user = User.includes(:fields).find(session[:user_id])
+      status_concluded = ExamStatusKind.find_by({name: 'Concluído'})
+      exam_ids = Exam.find_by_sql [open_exams_query, @user.fields.first.id]
+      @open_exams = Exam.where(id: exam_ids)
+      find_open_exams @user.fields.first.id
+      waiting_exams
+    end
   end
 
   def colors
@@ -23,7 +26,6 @@ class HomeUserController < ApplicationController
       "#DD4477", "#66AA00", "#B82E2E", "#316395", "#994499", "#22AA99", "#AAAA11",
       "#6633CC", "#E67300", "#8B0707", "#329262", "#5574A6", "#651067"
     ].shuffle
-    p chart_colors
     chart_colors
   end
 
@@ -58,7 +60,7 @@ class HomeUserController < ApplicationController
                  COUNT(oe.id) AS total
           FROM exams e
                INNER JOIN offered_exams oe ON oe.id = e.offered_exam_id
-          WHERE e.exam_status_kind_id <> (SELECT id FROM exam_status_kinds WHERE name = 'Aguardando início')
+          WHERE e.exam_status_kind_id = (SELECT id FROM exam_status_kinds WHERE name = 'Aguardando início')
                 AND oe.field_id = #{conn.quote(@user.fields.first.id)}
           GROUP BY oe.id;"
     @waiting_exams = {}
