1575002b81b254a7831ba4fb22a242f9d6920540
joao_felipe <joaofelipe1294@gmail.com>
2019-08-25 18:39:26 +0000

    Adicionadas anotacoes TODO do que iniciar segunda

--- spec/features/user/attendance/sample_validations_spec.rb
+++ spec/features/user/attendance/sample_validations_spec.rb
@@ -131,6 +131,6 @@ RSpec.feature "User::Attendance::SampleValidations", type: :feature do
   #   extract_subsample
   #   click_button id: 'sample_nav'
   #   expect(page).not_to have_selector("#btn-remove-sample")
-  # end
+  # end #FIXME corrigir teste
 
 end
