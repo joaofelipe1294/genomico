eab97d29acc531bf9c198238557a7a1866442c52
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-09-03 19:09:49 +0000

    Adicionado teste que valida o logout de usuário.

--- spec/features/user/home/logout_spec.rb
+++ spec/features/user/home/logout_spec.rb
@@ -9,11 +9,16 @@ RSpec.feature "User::Home::Logouts", type: :feature do
       password: Faker::Internet.password,
       name: Faker::Name.name,
       is_active: true,
-      fields: Field.IMUNOFENO # TODO: continuar daqui
+      fields: [Field.IMUNOFENO],
+      user_kind: UserKind.USER
     })
     visit root_path
-
-
+    fill_in "login", with: user.login
+    fill_in "password", with: user.password
+    click_button id: 'btn-login'
+    expect(page).to have_current_path home_user_index_path
+    click_link id: 'logout-link'
+    expect(page).to have_current_path root_path
   end
 
 end
