058a90c891bd464f4f3658e2dd3622aea714ded8
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-09-03 19:36:09 +0000

    Adicionados novos casos de teste a busca de paciente por nome.

--- spec/features/user/home/searches_spec.rb
+++ spec/features/user/home/searches_spec.rb
@@ -3,15 +3,32 @@ require 'helpers/user'
 
 RSpec.feature "User::Home::Searches", type: :feature do
 
-  it "search patient name" do
+  before :each do
     user_do_login_with_seeds
     create(:patient, name: 'Alien')
     create(:patient, name: 'Smooth ali')
     create(:patient,name: 'Smooth')
+  end
+
+  after :each do
+    expect(page).to have_current_path(patients_path, ignore_query: true)
+  end
+
+  it "search patient name" do
     fill_in 'patient-name-search', with: 'moo'
     click_button id: 'btn-search-patient'
-    expect(page).to have_current_path(patients_path, ignore_query: true)
     expect(find_all(class: 'patient').size).to eq 2
+  end
+
+  it "blank search" do
+    click_button id: 'btn-search-patient'
+    expect(find_all(class: 'patient').size).to eq Patient.all.size
+  end
+
+  it "search with no result" do
+    fill_in 'patient-name-search', with: 'langley'
+    click_button id: 'btn-search-patient'
+    expect(find_all(class: 'patient').size).to eq 0
   end
 
 end
