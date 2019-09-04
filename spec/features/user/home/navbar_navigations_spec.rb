192d4a9c643189624ace9f7b44ef2cf2b3f2a2e8
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-09-04 12:35:44 +0000

    Testadas navegações as views de atendimento.

--- spec/features/user/home/navbar_navigations_spec.rb
+++ spec/features/user/home/navbar_navigations_spec.rb
@@ -3,10 +3,13 @@ require 'helpers/user'
 
 RSpec.feature "User::Home::NavbarNavigations", type: :feature do
 
+  before :each do
+    user_do_login_with_seeds
+  end
+
   context "patient options" do
 
     before :each do
-      user_do_login_with_seeds
       click_link 'patient-dropdown'
     end
 
@@ -18,6 +21,47 @@ RSpec.feature "User::Home::NavbarNavigations", type: :feature do
     it "search" do
       click_link 'patients'
       expect(page).to have_current_path patients_path
+    end
+
+  end
+
+  context "attendance_options" do
+
+    before :each do
+      click_link 'attendance-dropdown'
+    end
+
+    it "panel" do
+      click_link 'attendance-panel'
+      expect(page).to have_current_path panels_exams_path
+    end
+
+    it "search" do
+      click_link 'attendance-search'
+      expect(page).to have_current_path attendances_path
+    end
+
+  end
+
+  context "samples" do
+
+    before :each do
+      click_link 'samples-dropdown'
+    end
+
+    it "imunofeno" do
+      click_link 'samples-imunofeno'
+      expect(page).to have_current_path internal_codes_path field_id: Field.IMUNOFENO
+    end
+
+    it "biomol" do
+      click_link 'samples-biomol'
+      expect(page).to have_current_path internal_codes_path field_id: Field.BIOMOL
+    end
+
+    it "fish" do
+      click_link 'samples-fish'
+      expect(page).to have_current_path internal_codes_path field_id: Field.FISH
     end
 
   end
