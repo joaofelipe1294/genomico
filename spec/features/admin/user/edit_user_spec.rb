98024573645f06c21ced3bd00e445fd34daa86f0
joao_felipe <joaofelipe1294@gmail.com>
2019-09-21 13:23:53 +0000

    Corrigidos testes que validam os modelos nanodrop_report e qubit_report.

--- spec/features/admin/user/edit_user_spec.rb
+++ spec/features/admin/user/edit_user_spec.rb
@@ -8,6 +8,7 @@ def navigate_to_edit_user_page
 end
 
 def setup
+	Rails.application.load_seed
 	create_users
 	admin_do_login
 	navigate_to_edit_user_page
--- spec/models/qubit_report_spec.rb
+++ spec/models/qubit_report_spec.rb
@@ -15,14 +15,23 @@ def setup
 		samples: [ sample ]
 	})
 	@attendance.save!(validate: false)
+	@subsample = Subsample.new({
+		sample: @attendance.samples.sample,
+		subsample_kind: SubsampleKind.all.sample,
+		collection_date: Date.today
+	})
 end
 
 RSpec.describe QubitReport, type: :model do
 
 	context 'Validations' do
 
+		before :each do
+			setup
+		end
+
 		it 'correct' do
-			qubit_report = create(:qubit_report, subsample: @attendance.samples.first)
+			qubit_report = create(:qubit_report, subsample: @subsample)
 			expect(qubit_report).to be_valid
 		end
 
@@ -33,7 +42,7 @@ RSpec.describe QubitReport, type: :model do
 		end
 
 		it 'without' do
-			qubit_report = build(:qubit_report, concentration: nil)
+			qubit_report = build(:qubit_report, subsample: @subsample, concentration: nil)
 			qubit_report.save
 			expect(qubit_report).to be_valid
 		end
