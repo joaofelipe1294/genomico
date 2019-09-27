c5bfa0474d7c8002323aa37dd71100ef58558037
joaofelipe-bio <joaofelipe1294@gmail.com>
2019-09-27 12:24:30 +0000

    Removida renderização do botão que gera um código interno para usuários que trabalham com subamostras.

--- app/helpers/subsamples_helper.rb
+++ app/helpers/subsamples_helper.rb
@@ -2,7 +2,7 @@ module SubsamplesHelper
 
   def subsamples_options_helper subsample
     options = ""
-    if User.find(session[:user_id]).fields.first != Field.IMUNOFENO
+    if User.find(session[:user_id]).fields.first != Field.IMUNOFENO && subsample.internal_codes.empty?
       options << link_to("Código interno", new_internal_code_path(subsample, target: "subsample"), method: :post, class: 'btn btn-sm btn-outline-secondary new-internal-code ml-3')
     end
     options << link_to('Editar', edit_subsample_path(subsample), class: 'btn btn-sm btn-outline-warning edit-subsample ml-3')
