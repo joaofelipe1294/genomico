7a3f184e124b00563633d633fd9f363dd1274cec
joao_felipe <joaofelipe1294@gmail.com>
2019-11-01 19:43:16 +0000

    Adicionada migração que reabre os atendimentos solicitados pelo FISH.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_31_194737) do
+ActiveRecord::Schema.define(version: 2019_11_01_192854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
