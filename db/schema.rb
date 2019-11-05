48cdab8d34c587f0d4ac44cd22d2998c2f63d5dc
joao_felipe <joaofelipe1294@gmail.com>
2019-11-05 19:34:51 +0000

    Alterada migração que adiciona dados retroativos a subsamples.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_123518) do
+ActiveRecord::Schema.define(version: 2019_11_05_192850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
