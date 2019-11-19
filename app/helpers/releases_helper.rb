5f21ae02579a7156e0fc9eb4bf3c4415e755ffa5
joao_felipe <joaofelipe1294@gmail.com>
2019-11-19 12:21:09 +0000

    Adicionado teste que valida a confirmação de mensagem de nova release.

--- app/helpers/releases_helper.rb
+++ app/helpers/releases_helper.rb
@@ -8,7 +8,7 @@ module ReleasesHelper
                         .first
     message = "Disponibilizada nova versão #{release_check.release.tag}. "
     message << "#{release_check.release.message}"
-    message << link_to('Ok', check_release_message_path(release_check), method: :patch, class: "float-right btn btn-outline-success").html_safe
+    message << link_to('Ok', check_release_message_path(release_check), method: :patch, class: "float-right btn btn-outline-success", id: "confirm-release").html_safe
     message.html_safe
   end
 
---
+++ spec/features/admin/release/confirm_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.feature "Admin::Release::Confirms", type: :feature do
+  include UserLogin
+
+  it "confirma new release", js: false do
+    Rails.application.load_seed
+    biomol_user_do_login
+    create(:release)
+    visit current_path
+    expect(page).to have_selector('#release-message', visible: true)
+    click_link id: "confirm-release"
+    expect(page).not_to have_selector('#release-message')
+  end
+
+end
