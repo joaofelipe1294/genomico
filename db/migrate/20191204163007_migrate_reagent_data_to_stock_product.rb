ed18be78ba5a4a8f6fb84112b34b7cf78cb73c59
joao_felipe <joaofelipe1294@gmail.com>
2019-12-04 16:57:32 +0000

    Adicionado create na migração que adiciona os dados de reagentes em stock_products

--- db/migrate/20191204163007_migrate_reagent_data_to_stock_product.rb
+++ db/migrate/20191204163007_migrate_reagent_data_to_stock_product.rb
@@ -2,7 +2,7 @@ class MigrateReagentDataToStockProduct < ActiveRecord::Migration[5.2]
   def change
     Reagent.all.each do |reagent|
       p reagent
-      stock_item = StockProduct.new({
+      stock_item = StockProduct.create({
         name: reagent.name,
         usage_per_test: reagent.usage_per_test,
         total_aviable: reagent.total_aviable,
