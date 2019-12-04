dff0d03f346303d0ce9c16991e705fcaf47b426d
joao_felipe <joaofelipe1294@gmail.com>
2019-12-04 16:46:14 +0000

    Adicionada migração que adapta os dados de reagete para stock-product.

---
+++ db/migrate/20191204163007_migrate_reagent_data_to_stock_product.rb
@@ -0,0 +1,23 @@
+class MigrateReagentDataToStockProduct < ActiveRecord::Migration[5.2]
+  def change
+    Reagent.all.each do |reagent|
+      p reagent
+      stock_item = StockProduct.new({
+        name: reagent.name,
+        usage_per_test: reagent.usage_per_test,
+        total_aviable: reagent.total_aviable,
+        first_warn_at: reagent.first_warn_at,
+        danger_warn_at: reagent.danger_warn_at,
+        mv_code: reagent.mv_code,
+        unit_of_measurement: reagent.unit_of_measurement,
+        field: reagent.field,
+      })
+      if reagent.field.nil?
+        stock_item.is_shared = true
+      else
+        stock_item.is_shared = false
+      end
+      p stock_item
+    end
+  end
+end
