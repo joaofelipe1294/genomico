712e3fd0f12106b265c09dbe9cb25e7d9f09e02f
joao_felipe <joaofelipe1294@gmail.com>
2019-12-16 17:23:39 +0000

    Adicionados testes que validam o cadastro de uma nova entrada de estoque.

--- spec/features/user/stock_entry/new_spec.rb
+++ spec/features/user/stock_entry/new_spec.rb
@@ -1,5 +1,55 @@
 require 'rails_helper'
 
-RSpec.feature "User::StockEntry::News", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "User::StockEntry::News", type: :feature, js: true do
+  include UserLogin
+  include ValidationChecks
+
+  before :each do
+    Rails.application.load_seed
+    create(:user)
+    create(:brand)
+    stock_product = create(:stock_product)
+    biomol_user_do_login
+    click_link id: "stock-dropdown"
+    click_link id: "new-stock-entry"
+    fill_in "stock_entry[entry_date]", with: 3.days.ago
+    select(stock_product.field.name, from: "fields-select").select_option
+    fill_in "stock_entry[product_attributes][lot]", with: "981273kjasbd"
+    fill_in "stock_entry[product_attributes][shelf_life]", with: 2.months.from_now
+    fill_in "stock_entry[product_attributes][amount]", with: "2"
+    fill_in "stock_entry[product_attributes][location]", with: "Some locale"
+  end
+
+  def success
+    click_button id: "btn-save"
+    expect(page).to have_current_path display_new_tag_path(StockEntry.all.sample)
+    expect(find(id: "success-warning").text).to eq I18n.t :new_stock_entry_success
+  end
+
+  it "complete with label" do
+    success
+  end
+
+  it "complete without label" do
+    choose 'stock_entry[product_attributes][has_tag]', option: "false"
+    click_button id: "btn-save"
+    expect(page).to have_current_path stock_entries_path
+    expect(find(id: "success-warning").text).to eq I18n.t :new_stock_entry_success
+  end
+
+  it "without lot" do
+    fill_in "stock_entry[product_attributes][lot]", with: ""
+    without_value "Lote"
+  end
+
+  it "without shelf_life" do
+    choose "stock_entry[product_attributes][has_shelf_life]", option: "false"
+    success
+  end
+
+  it "without amount" do
+    fill_in "stock_entry[product_attributes][amount]", with: ""
+    without_value "Quantidade"
+  end
+
 end
--- spec/support/validation_checks.rb
+++ spec/support/validation_checks.rb
@@ -2,8 +2,8 @@ module ValidationChecks
 
   def success_check destny_path, message_key
     click_button id: "btn-save"
-    expect(page).to have_current_path destny_path
     expect(find(id: "success-warning").text).to eq I18n.t message_key
+    expect(page).to have_current_path destny_path
   end
 
   def without_value attribute_name
