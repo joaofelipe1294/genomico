3281803f6f409a57da57c37e5b9e020aaa32d3d8
joao_felipe <joaofelipe1294@gmail.com>
2019-12-27 18:24:36 +0000

    Adicionado teste que valida indicação de próximo produto a ser aberto.

--- spec/features/user/stock_out/new_spec.rb
+++ spec/features/user/stock_out/new_spec.rb
@@ -3,13 +3,22 @@ require 'rails_helper'
 RSpec.feature "User::StockOut::News", type: :feature do
   include UserLogin
 
-  it "new stock_out", js: true do
+  def do_stock_out_on_first_product
+    @product.change_to_in_use({date: Date.current})
+    visit new_stock_out_path(@product)
+    click_button id: "btn-save"
+  end
+
+  before :each do
     Rails.application.load_seed
-    create(:brand)
     create(:user, user_kind: UserKind.USER)
-    stock_product = create(:stock_product)
-    product = create(:product, stock_product: stock_product)
-    create(:stock_entry, product: product)
+    create(:brand)
+    @stock_product = create(:stock_product)
+    @product = create(:product, stock_product: @stock_product)
+    @stock_entry = create(:stock_entry, product: @product)
+  end
+
+  it "new stock_out", js: true do
     imunofeno_user_do_login
     click_link id: "stock-dropdown"
     click_link id: "in-stock-products"
@@ -26,7 +35,21 @@ RSpec.feature "User::StockOut::News", type: :feature do
     click_link id: "stock-outs"
     expect(page).to have_current_path stock_outs_path
     expect(find_all(class: "stock-out").size).to eq 1
-    # sleep 10
+  end
+
+  it "with one product in stock" do
+    imunofeno_user_do_login
+    second_product = create(:product, stock_product: @stock_product, shelf_life: 2.weeks.from_now)
+    create(:stock_entry, product: second_product)
+    do_stock_out_on_first_product
+    expect(page).to have_current_path next_product_to_open_path(second_product)
+  end
+
+  it "without stock_product" do
+    imunofeno_user_do_login
+    do_stock_out_on_first_product
+    expect(find(id: "danger-warning").text).to eq I18n.t :without_product_to_open_in_stock
+    expect(page).to have_current_path stock_outs_path
   end
 
 end
