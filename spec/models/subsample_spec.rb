48a93f1653ef12bd56faeb9efa4b324e9d9485f0
joao_felipe <joaofelipe1294@gmail.com>
2020-01-02 12:21:42 +0000

    Repaginada geração de código interno.

--- spec/models/subsample_spec.rb
+++ spec/models/subsample_spec.rb
@@ -90,7 +90,8 @@ RSpec.describe Subsample, type: :model do
 			)
 			subsample = Subsample.find subsample.id
 			expect(subsample).to be_valid
-			expect(subsample.refference_label).to eq("19-DNA-0419")
+			year = Date.today.year.to_s.slice(2, 3)
+			expect(subsample.refference_label).to eq("#{year}-DNA-0001")
 		end
 
 	end
