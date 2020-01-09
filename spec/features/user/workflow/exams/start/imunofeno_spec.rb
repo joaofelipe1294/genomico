846a974f369cf7dc5f84d5daed345040571960b6
joaofelipe <joaofelipe1294@gmail.com>
2020-01-09 11:52:04 +0000

    Adicionados testes que validam o inicio de um exame por usuário da imunofeno.

--- spec/features/user/workflow/exams/start/imunofeno_spec.rb
+++ spec/features/user/workflow/exams/start/imunofeno_spec.rb
@@ -3,20 +3,29 @@ require 'rails_helper'
 RSpec.feature "User::Workflow::Exams::Start::Imunofenos", type: :feature do
   include AttendanceHelper
   include UserLogin
+  include ValidationChecks
+
+  before :each do
+    Rails.application.load_seed
+    @attendance = create_raw_imunofeno_attendance
+    imunofeno_user_do_login
+    visit workflow_path(@attendance, {tab: "exams"})
+  end
 
   it "start exam with internal code" do
-    Rails.application.load_seed
-    attendance = create_raw_imunofeno_attendance
-    imunofeno_user_do_login
-    visit workflow_path(attendance, {tab: "exams"})
     click_link id: "samples-nav"
     click_link class: "new-internal-code", match: :first
     click_link id: "exams-nav"
-    
-    # p Exam.all.sample
-    # p ExamStatusKind.WAITING_START
-    # puts page.html
-    # click_link class: "start-exam", match: :first
+    click_link class: "start-exam", match: :first
+    expect(page).to have_current_path start_exam_path(@attendance.exams.first)
+    click_button id: "btn-save"
+    expect(find(id: "success-warning").text).to eq "Status de exame alterado para Em andamento."
+    expect(page).to have_current_path workflow_path(@attendance, {tab: 'exams'})
+  end
+
+  it "try start exam without internal code" do
+    click_link class: "start-exam", match: :first
+    expect(find(id: "without-sample")).to be_visible
   end
 
 end
