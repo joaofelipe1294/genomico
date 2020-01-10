e1513caa9e026b0dab9967017f634642ca50b045
joaofelipe <joaofelipe1294@gmail.com>
2020-01-10 14:51:40 +0000

    Adicionados testes que validam a criação e remoção de códigos internos.

---
+++ spec/features/user/workflow/samples/internal_code_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.feature "User::Workflow::Samples::InternalCodes", type: :feature do
+  include AttendanceHelper
+  include UserLogin
+
+  before :each do
+    Rails.application.load_seed
+    @attendance = create_raw_imunofeno_attendance
+    imunofeno_user_do_login
+    visit workflow_path(@attendance, tab: 'samples')
+  end
+
+  it "add imunofeno internal code" do
+    expect(find_all(class: "internal-code").size).to eq 0
+    click_link class: "new-internal-code", match: :first
+    expect(page).to have_current_path workflow_path(@attendance, tab: 'samples')
+    expect(find_all(class: "internal-code").size).to eq 1
+  end
+
+  it "remove imunofeno internal code", js: true do
+    click_link class: "new-internal-code", match: :first
+    page.driver.browser.accept_confirm
+    click_link class: "remove-internal-code"
+    expect(page).to have_current_path workflow_path(@attendance, tab: "samples")
+    expect(find_all(class: "internal-code").size).to eq 0
+  end
+
+end
--- spec/helpers/user.rb
+++ spec/helpers/user.rb
@@ -1,80 +1,80 @@
-def user_do_login
-	user_kind = UserKind.create({name: 'user'})
-		user = User.create({
-			login: Faker::Internet.username,
-			name: Faker::Name.name,
-			password: '1234',
-			user_kind: user_kind
-		})
-		visit(root_path)
-		fill_in('login', with: user.login)
-		fill_in('password', with: user.password)
-		click_button(class: 'btn')
-		expect(page).to have_current_path home_user_index_path
-end
-
-def do_login
-	visit(root_path)
-	fill_in('login', with: 'user')
-	fill_in('password', with: '1234')
-	click_button(class: 'btn')
-	expect(page).to have_current_path home_user_index_path
-end
-
-def user_do_login_with_seeds field: nil
-	Rails.application.load_seed
-	field = Field.IMUNOFENO if field.nil?
-	user = User.create({
-		login: Faker::Internet.username,
-		password: Faker::Internet.password,
-		name: Faker::Name.name,
-		is_active: true,
-		fields: [field],
-		user_kind: UserKind.USER
-	})
-	visit root_path
-	fill_in "login", with: user.login
-	fill_in "password", with: user.password
-	click_button id: 'btn-login'
-end
-
-def imunofeno_user_do_login_with_seeds
-	user_do_login_with_seeds field: Field.IMUNOFENO
-end
-
-def biomol_user_do_login_with_seeds
-	user_do_login_with_seeds field: Field.BIOMOL
-end
-
-def imunofeno_user_do_login
-	field = Field.IMUNOFENO if field.nil?
-	@user = User.create({
-		login: Faker::Internet.username,
-		password: Faker::Internet.password,
-		name: Faker::Name.name,
-		is_active: true,
-		fields: [field],
-		user_kind: UserKind.USER
-	})
-	login
-end
-
-def biomol_user_do_login
-	field = Field.BIOMOL if field.nil?
-	@user = User.create({
-		login: Faker::Internet.username,
-		password: Faker::Internet.password,
-		name: Faker::Name.name,
-		is_active: true,
-		fields: [field],
-		user_kind: UserKind.USER
-	})
-	login
-end
-
-def login
-	visit root_path
-	fill_in "login", with: @user.login
-	fill_in "password", with: @user.password
-	click_button id: 'btn-login'
-end
+# def user_do_login
+# 	user_kind = UserKind.create({name: 'user'})
+# 		user = User.create({
+# 			login: Faker::Internet.username,
+# 			name: Faker::Name.name,
+# 			password: '1234',
+# 			user_kind: user_kind
+# 		})
+# 		visit(root_path)
+# 		fill_in('login', with: user.login)
+# 		fill_in('password', with: user.password)
+# 		click_button(class: 'btn')
+# 		expect(page).to have_current_path home_user_index_path
+# end
+#
+# def do_login
+# 	visit(root_path)
+# 	fill_in('login', with: 'user')
+# 	fill_in('password', with: '1234')
+# 	click_button(class: 'btn')
+# 	expect(page).to have_current_path home_user_index_path
+# end
+#
+# def user_do_login_with_seeds field: nil
+# 	Rails.application.load_seed
+# 	field = Field.IMUNOFENO if field.nil?
+# 	user = User.create({
+# 		login: Faker::Internet.username,
+# 		password: Faker::Internet.password,
+# 		name: Faker::Name.name,
+# 		is_active: true,
+# 		fields: [field],
+# 		user_kind: UserKind.USER
+# 	})
+# 	visit root_path
+# 	fill_in "login", with: user.login
+# 	fill_in "password", with: user.password
+# 	click_button id: 'btn-login'
+# end
+#
+# def imunofeno_user_do_login_with_seeds
+# 	user_do_login_with_seeds field: Field.IMUNOFENO
+# end
+#
+# def biomol_user_do_login_with_seeds
+# 	user_do_login_with_seeds field: Field.BIOMOL
+# end
+#
+# def imunofeno_user_do_login
+# 	field = Field.IMUNOFENO if field.nil?
+# 	@user = User.create({
+# 		login: Faker::Internet.username,
+# 		password: Faker::Internet.password,
+# 		name: Faker::Name.name,
+# 		is_active: true,
+# 		fields: [field],
+# 		user_kind: UserKind.USER
+# 	})
+# 	login
+# end
+#
+# def biomol_user_do_login
+# 	field = Field.BIOMOL if field.nil?
+# 	@user = User.create({
+# 		login: Faker::Internet.username,
+# 		password: Faker::Internet.password,
+# 		name: Faker::Name.name,
+# 		is_active: true,
+# 		fields: [field],
+# 		user_kind: UserKind.USER
+# 	})
+# 	login
+# end
+#
+# def login
+# 	visit root_path
+# 	fill_in "login", with: @user.login
+# 	fill_in "password", with: @user.password
+# 	click_button id: 'btn-login'
+# end
