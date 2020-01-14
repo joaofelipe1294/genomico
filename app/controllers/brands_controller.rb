670fd5a623fe96ea71ecd62b18422a03e26902c2
joaofelipe <joaofelipe1294@gmail.com>
2020-01-14 14:52:33 +0000

    Adicionada paginação na listagem de marca.

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -4,7 +4,7 @@ class BrandsController < ApplicationController
   # GET /brands
   # GET /brands.json
   def index
-    @brands = Brand.all.order name: :asc
+    @brands = Brand.all.order(name: :asc).page params[:page]
   end
 
   # GET /brands/new
--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,4 +1,5 @@
 class Brand < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: true
+  paginates_per 14
 end
