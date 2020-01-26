2c90f0dcefe1e2f4446170051e5b3b43da3a86f9
joaofelipe <joaofelipe1294@gmail.com>
2020-01-26 22:09:54 +0000

    Extraído hash com os estados possíveis de uma suggestion para um concern.

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -1,17 +1,11 @@
 class Suggestion < ApplicationRecord
+  include SuggestionStatuses
   belongs_to :requester, class_name: :User
   validates_presence_of :title, :description, :requester, :current_status, :kind
   validates_uniqueness_of :title
   before_validation :set_default_status
   has_many :suggestion_progresses
-  enum current_status: {
-    in_line: 0,
-    evaluating: 1,
-    in_progress: 2,
-    waiting_validation: 3,
-    complete: 4,
-    canceled: 5
-  }
+  enum current_status: statuses()
   enum kind: {
     bug: 0,
     new_feature: 1,
--- app/models/suggestion_progress.rb
+++ app/models/suggestion_progress.rb
@@ -1,4 +1,5 @@
 class SuggestionProgress < ApplicationRecord
   belongs_to :suggestion
   belongs_to :responsible, class_name: :User
+  enum current_status: statuses()
 end
