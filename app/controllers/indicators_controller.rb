a5ca62481e7706ec5d56aa8ec3f3aa51b47e634a
joaofelipe <joaofelipe1294@gmail.com>
2020-01-29 20:30:37 +0000

    Refatorada indicators controller.

--- app/controllers/indicators_controller.rb
+++ app/controllers/indicators_controller.rb
@@ -1,54 +1,36 @@
 class IndicatorsController < ApplicationController
   include ResponseTimeReport
+  before_action :set_exams, only: [:exams_in_progress, :concluded_exams, :health_ensurances_relation]
 
   def exams_per_field
   end
 
   def exams_in_progress
-    exams = Exam
-                .joins(:offered_exam)
-                .where.not(exam_status_kind: ExamStatusKind.COMPLETE)
-                .where.not(exam_status_kind: ExamStatusKind.CANCELED)
+    exams = Exam.joins(offered_exam: [:field]).where.not(exam_status_kind: [ExamStatusKind.CANCELED, ExamStatusKind.COMPLETE])
     @exams_in_progress_count = exams.size
-    @exams_relation = generate_fields_relation exams
+    @exams_relation = exams.group("fields.name").count
   end
 
   def concluded_exams
-    complete_exams = Exam.joins(:offered_exam).where(exam_status_kind: ExamStatusKind.COMPLETE)
-    complete_exams = filter_by_date complete_exams
-    @concluded_exams_cont = complete_exams.size
-    @complete_exams_relation = generate_fields_relation complete_exams
+    @concluded_exams_cont = @exams.size
+    @complete_exams_relation = @exams.joins(offered_exam: [:field]).group("fields.name").count
   end
 
   def health_ensurances_relation
-    exams = Exam.complete.joins(:attendance)
-    exams = exams.where("exams.finish_date BETWEEN ? AND ?", params[:start_date], params[:end_date]) if filter_by_date
-    @concluded_exams_cont = exams.size
-    @exams_relation = exams.joins(attendance: [:health_ensurance]).complete.group("health_ensurances.name").count
+    @concluded_exams_cont = @exams.size
+    @exams_relation = @exams.joins(attendance: [:health_ensurance]).complete.group("health_ensurances.name").count
   end
 
   private
 
-  def filter_by_date exams
+  def set_exams
     start_date = params[:start_date]
     end_date = params[:end_date]
-    if start_date.present? && end_date.present?
-      exams = exams.where("exams.finish_date BETWEEN ? AND ?", start_date, end_date)
-    end
-    exams
-  end
-
-  def generate_fields_relation exams
-    exams_relation = {}
-    Field.all.order(:name).each do |field|
-      amount = exams.where("offered_exams.field_id = ?", field.id).size
-      exams_relation[field.name] = amount if amount > 0
-    end
-    exams_relation
-  end
-
-  def filter_by_date
-    params[:start_date].present? && params[:end_date].present?
+    @exams = Exam.complete.joins(offered_exam: [:field])
+    @exams = @exams.where("exams.finish_date BETWEEN ? AND ?", start_date, end_date) if start_date && end_date
   end
 
 end
+
+
+# TODO: Continuar refatorando indicadores atuais ...
