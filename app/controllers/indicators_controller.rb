67f1db299a9f061db081138fa096640a85aa2e14
joaofelipe <joaofelipe1294@gmail.com>
2020-01-29 20:08:01 -0300

    Removida dependência da variavel de instância @exams em indicators_controller.

--- app/controllers/indicators_controller.rb
+++ app/controllers/indicators_controller.rb
@@ -12,13 +12,15 @@ class IndicatorsController < ApplicationController
   end
 
   def concluded_exams
-    @concluded_exams_cont = @exams.size
-    @complete_exams_relation = @exams.joins(offered_exam: [:field]).group("fields.name").count
+    exams = set_exams
+    @concluded_exams_cont = exams.size
+    @complete_exams_relation = exams.joins(offered_exam: [:field]).group("fields.name").count
   end
 
   def health_ensurances_relation
-    @concluded_exams_cont = @exams.size
-    @exams_relation = @exams.joins(attendance: [:health_ensurance]).complete.group("health_ensurances.name").count
+    exams = set_exams
+    @concluded_exams_cont = exams.size
+    @exams_relation = exams.joins(attendance: [:health_ensurance]).complete.group("health_ensurances.name").count
   end
 
   private
@@ -26,11 +28,9 @@ class IndicatorsController < ApplicationController
   def set_exams
     start_date = params[:start_date]
     end_date = params[:end_date]
-    @exams = Exam.complete.joins(offered_exam: [:field])
-    @exams = @exams.where("exams.finish_date BETWEEN ? AND ?", start_date, end_date) if start_date && end_date
+    exams = Exam.complete.joins(offered_exam: [:field])
+    exams = exams.where("exams.finish_date BETWEEN ? AND ?", start_date, end_date) if start_date && end_date
+    exams
   end
 
 end
-
-
-# TODO: Continuar refatorando indicadores atuais ...
