813b0fbdd8e60944cd71e70451af6d0dd17fbc51
joao_felipe <joaofelipe1294@gmail.com>
2020-01-30 17:05:01 -0300

    Refatorada home do user, alterada lógica de busca dos dados exibidos na home.

--- app/controllers/home_user_controller.rb
+++ app/controllers/home_user_controller.rb
@@ -3,33 +3,9 @@ class HomeUserController < ApplicationController
 
   def index
     @user = User.includes(:fields).find session[:user_id]
-    @offered_exams = OfferedExam.where(field: @user.fields.first).where(is_active: true).order name: :asc
-    @exam_status_kinds = ExamStatusKind.all.order(name: :asc)
-    unless @user.fields.empty?
-      @issues = helpers.find_issues filter_by: params
-      @waiting_exams = helpers.waiting_exams @issues
-      @exams_in_progress = helpers.exams_in_progress @issues
-      @delayed_exams = find_delayed_exams
-    end
+    @report = HomeUserReport.new({field: @user.field})
+    @offered_exams = OfferedExam.from_field(@user.field).order name: :asc
+    @issues = @report.find_issues filter_by: params
   end
-
-  private
-
-    def find_delayed_exams
-      exams = @issues
-                  .where.not(exam_status_kind: ExamStatusKind.COMPLETE)
-                  .where.not(exam_status_kind: ExamStatusKind.CANCELED)
-      count = 0
-      exams_relation = {}
-      offered_exams = exams.map { |exam| exam.offered_exam }.uniq
-      offered_exams.each do |offered_exam|
-        open_exams = exams.where(offered_exam: offered_exam)
-        total_late_exams = 0
-        open_exams.each { |exam| total_late_exams += 1 if exam.is_late?}
-        exams_relation[offered_exam.name] = total_late_exams
-        count += total_late_exams
-      end
-      { count: count, relation: exams_relation }
-    end
 
 end
--- app/helpers/home_user_helper.rb
+++ app/helpers/home_user_helper.rb
@@ -1,61 +1,18 @@
 module HomeUserHelper
 
-  def waiting_exams exams
-    exams_waiting_to_start = exams.where(exam_status_kind: ExamStatusKind.WAITING_START)
-    waiting_exams_count = exams_waiting_to_start.size
-    exams_relation = exams_waiting_to_start.group(:offered_exam).size
-    relation = {}
-    exams_relation.keys.each { |offered_exam| relation[offered_exam.name] = exams_relation[offered_exam] }
-    { count: waiting_exams_count, relation: relation }
-  end
-
-  def exams_in_progress exams
-    exams_in_progress = exams
-                            .where.not(exam_status_kind: ExamStatusKind.WAITING_START)
-                            .where.not(exam_status_kind: ExamStatusKind.COMPLETE)
-                            .where.not(exam_status_kind: ExamStatusKind.CANCELED)
-    exams_in_progress_count = exams_in_progress.size
-    exams_relation = exams_in_progress.group(:offered_exam).size
-    relation = {}
-    exams_relation.keys.each do |offered_exam|
-      relation[offered_exam.name] = exams_relation[offered_exam]
-    end
-    { count: exams_in_progress_count , relation: relation }
-  end
-
-  def find_issues filter_by: nil
-    issues = Exam
-                .joins(:offered_exam, :exam_status_kind)
-                .where.not(exam_status_kind: ExamStatusKind.COMPLETE)
-                .where.not(exam_status_kind: ExamStatusKind.CANCELED)
-                .where("offered_exams.field_id = ?", @user.fields.first)
-                .includes(:offered_exam, :internal_codes, :exam_status_kind, attendance: [:patient])
-                .order(created_at: :asc)
-    if filter_by[:exam_status_kind].present?
-      if filter_by[:exam_status_kind] == "waiting_start"
-        issues = issues.where(exam_status_kind: ExamStatusKind.WAITING_START)
-      elsif filter_by[:exam_status_kind] == "in_progress"
-        issues = issues.where.not(exam_status_kind: ExamStatusKind.WAITING_START)
-      else
-        delayed_exams = []
-        issues.each { |exam| delayed_exams << exam if exam.is_late? }
-        issues = Exam.where(id: delayed_exams.map {|exam| exam.id})
-      end
-    elsif filter_by[:offered_exam].present? && filter_by[:offered_exam] != 'Todos'
-      issues = issues.where(offered_exam_id: filter_by[:offered_exam])
-    else
-      cache = Rails.cache.read "exams:field:#{@user.fields.first.name}"
-      if cache.nil? == false && Rails.env != "test"
-        issues = cache
-      else
-        issues = @user.fields.first.set_issues_in_cache
-      end
-    end
-    issues
-  end
-
-
-
+  # def exams_in_progress exams
+  #   exams_in_progress = exams
+  #                           .where.not(exam_status_kind: ExamStatusKind.WAITING_START)
+  #                           .where.not(exam_status_kind: ExamStatusKind.COMPLETE)
+  #                           .where.not(exam_status_kind: ExamStatusKind.CANCELED)
+  #   exams_in_progress_count = exams_in_progress.size
+  #   exams_relation = exams_in_progress.group(:offered_exam).size
+  #   relation = {}
+  #   exams_relation.keys.each do |offered_exam|
+  #     relation[offered_exam.name] = exams_relation[offered_exam]
+  #   end
+  #   { count: exams_in_progress_count , relation: relation }
+  # end
 
   def chart_colors
     {
