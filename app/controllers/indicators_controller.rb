3f6fe8fb951c6eb326ff4434b82b627df3c537af
joaofelipe <joaofelipe1294@gmail.com>
2020-01-30 13:34:28 +0000

    Refatorada indicators_controller.

--- app/controllers/indicators_controller.rb
+++ app/controllers/indicators_controller.rb
@@ -1,26 +1,22 @@
 class IndicatorsController < ApplicationController
   before_action :user_filter
+  before_action :set_exams, only: [:concluded_exams, :health_ensurances_relation]
+  before_action :set_offered_exam_group, only: [:response_time]
 
   def exams_in_progress
-    exams = Exam.joins(offered_exam: [:field]).where.not(exam_status_kind: [ExamStatusKind.CANCELED, ExamStatusKind.COMPLETE])
-    @exams_in_progress_count = exams.size
-    @exams_relation = exams.group("fields.name").count
+    @exams = Exam.joins(offered_exam: [:field]).where.not(exam_status_kind: [ExamStatusKind.CANCELED, ExamStatusKind.COMPLETE])
+    @relation = @exams.group("fields.name").count
   end
 
   def concluded_exams
-    exams = set_exams
-    @concluded_exams_cont = exams.size
-    @complete_exams_relation = exams.joins(offered_exam: [:field]).group("fields.name").count
+    @relation = @exams.joins(offered_exam: [:field]).group("fields.name").count
   end
 
   def health_ensurances_relation
-    exams = set_exams
-    @concluded_exams_cont = exams.size
-    @exams_relation = exams.joins(attendance: [:health_ensurance]).complete.group("health_ensurances.name").count
+    @relation = @exams.joins(attendance: [:health_ensurance]).complete.group("health_ensurances.name").count
   end
 
   def response_time
-    @offered_exam_group = OfferedExamGroup.find params[:id]
     @report = ResponseTimeReport.new({offered_exam_group: @offered_exam_group, start_date: params[:start_date], finish_date: params[:end_date]})
   end
 
@@ -30,12 +26,15 @@ class IndicatorsController < ApplicationController
 
   private
 
-  def set_exams
-    start_date = params[:start_date]
-    end_date = params[:end_date]
-    exams = Exam.complete.joins(offered_exam: [:field])
-    exams = exams.where("exams.finish_date BETWEEN ? AND ?", start_date, end_date) if start_date && end_date
-    exams
-  end
+    def set_exams
+      start_date = params[:start_date]
+      end_date = params[:end_date]
+      @exams = Exam.complete.joins(offered_exam: [:field])
+      @exams = @exams.where("exams.finish_date BETWEEN ? AND ?", start_date, end_date) if start_date && end_date
+    end
+
+    def set_offered_exam_group
+      @offered_exam_group = OfferedExamGroup.find params[:id]
+    end
 
 end
