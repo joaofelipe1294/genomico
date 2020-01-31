4344b10f951b876f6dbce60f92c4482616ba1e0b
joaofelipe <joaofelipe1294@gmail.com>
2020-01-31 20:04:07 +0000

    Adicionada migration que trata os dados de attendance_status_kind, e remove a tabela.

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,7 +1,7 @@
 class Attendance < ActiveRecord::Base
   belongs_to :desease_stage
   belongs_to :patient
-  belongs_to :attendance_status_kind
+  # belongs_to :attendance_status_kind
   belongs_to :health_ensurance
   has_many :exams
   has_many :samples
@@ -9,7 +9,7 @@ class Attendance < ActiveRecord::Base
   accepts_nested_attributes_for :samples
   accepts_nested_attributes_for :exams
   before_validation :default_values
-  validates :desease_stage, :lis_code, :patient, :exams, :samples, :attendance_status_kind, presence: true
+  validates :desease_stage, :lis_code, :patient, :exams, :samples, :status, presence: true
   validates :lis_code, uniqueness: true
   has_attached_file :report
   validates_attachment_content_type :report, :content_type => ["application/pdf"]
@@ -17,6 +17,10 @@ class Attendance < ActiveRecord::Base
   paginates_per 10
   has_many :internal_codes
   after_create :update_cache
+  enum status: {
+    progress: 1,
+    complete: 2
+  }
 
   def conclude
     self.finish_date = Date.today
@@ -34,13 +38,13 @@ class Attendance < ActiveRecord::Base
 
   def default_values
     self.start_date = Date.today if self.start_date.nil?
-    self.attendance_status_kind = AttendanceStatusKind.IN_PROGRESS if self.attendance_status_kind.nil?
+    self.status = :progress if self.status.nil?
   end
 
   def update_cache
     Field.IMUNOFENO.set_issues_in_cache
     Field.BIOMOL.set_issues_in_cache
     Field.FISH.set_issues_in_cache
-  end
+  end  # TODO: extrair para service isolado ou modulo que gerencie o cache, talves se aplique a field !!!
 
 end
