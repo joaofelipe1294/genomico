9f7891a3f508ee74174c1019a2f116634cc223ad
joaofelipe <joaofelipe1294@gmail.com>
2020-01-31 19:37:24 +0000

    Corrigida migration que migra os dados da tabela user_kind para a nova coluna.

--- db/migrate/20200131122009_remove_user_kinds.rb
+++ db/migrate/20200131122009_remove_user_kinds.rb
@@ -1,7 +1,7 @@
 class RemoveUserKinds < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :kind, :integer
-    User.all.each { |user| user.update kind: user.user_kind.id }
+    User.all.each { |user| user.update kind: user.user_kind_id }
     remove_column :users, :user_kind_id
     drop_table :user_kinds
   end
