d1561ef6f1dbfc132bd3d0f4443deaccfa88015d
joaofelipe <joaofelipe1294@gmail.com>
2020-02-02 12:59:03 +0000

    Adicionada enum com as unidades de medida cadastradas.

--- app/models/stock_product.rb
+++ app/models/stock_product.rb
@@ -1,5 +1,5 @@
 class StockProduct < ApplicationRecord
-  belongs_to :unit_of_measurement
+  # belongs_to :unit_of_measurement
   belongs_to :field
   validates :name, presence: true
   validates :name, uniqueness: true
@@ -7,6 +7,24 @@ class StockProduct < ApplicationRecord
   validates_with StockProductMvCodeUniquenessValidator
   paginates_per 15
   has_many :products
+
+  enum unit_of_measurement: {
+    boxes: 1,
+    microliters: 2,
+    unities: 3,
+    kits: 4,
+    reactions: 5,
+    extractions: 6,
+    tests: 7,
+    separations: 8,
+    mililiters: 9,
+    liters: 10,
+    gram: 11,
+    miligram: 12,
+    pipe: 13,
+    bottle: 14,
+    package: 15
+  }
 
   def self.products_base_report
     StockProduct.includes(:unit_of_measurement).all.order(:name).select do |stock_product|
