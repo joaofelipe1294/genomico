7ef936207b4887dd767ca58d6d4971404d9d2481
joaofelipe <joaofelipe1294@gmail.com>
2020-03-26 18:20:49 +0000

    Updated gem nokogiri

--- backup_cron.sh
+++ backup_cron.sh
@@ -1,8 +1,2 @@
-#adicionar permissao de execucao
-# chmod +x backup_cron.sh
-#adicionar ao cron
-# * */2 * * * /opt/genomico/backup_cron.sh
-
-
 cd "/opt/genomico"
 /home/joao/.rbenv/shims/rails backup:do RAILS_ENV=production
