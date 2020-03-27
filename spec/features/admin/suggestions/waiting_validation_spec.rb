a2d5fc8f44f92a6f96b8f30f90f3330a10763ded
joaofelipe <joaofelipe1294@gmail.com>
2020-03-27 12:17:29 +0000

    Fixed all features/admin/suggestions spec

--- spec/features/admin/suggestions/waiting_validation_spec.rb
+++ spec/features/admin/suggestions/waiting_validation_spec.rb
@@ -24,10 +24,8 @@ RSpec.feature "Admin::Suggestions::WaitingValidations", type: :feature do
 
       before :each do
         admin = User.where(kind: :admin).first
-        @suggestion.change_status :evaluating, admin
+        @suggestion.update current_status: :development
         expect(@suggestion.reload.suggestion_progresses.size).to match 2
-        @suggestion.change_to_development admin, 5
-        expect(@suggestion.reload.suggestion_progresses.size).to match 3
         visit suggestions_index_admin_path(kind: :in_progress)
       end
 
@@ -37,12 +35,10 @@ RSpec.feature "Admin::Suggestions::WaitingValidations", type: :feature do
 
       context "when click in change to waiting_validation link" do
 
-        before :each do
-          click_link class: "change-to-waiting-validation", match: :first
-        end
+        before(:each) { click_link class: "change-to-waiting-validation", match: :first }
 
         it "is expected to generate a new suggestion_progress" do
-          expect(@suggestion.reload.suggestion_progresses.size).to match 4
+          expect(@suggestion.reload.suggestion_progresses.size).to match 3
         end
 
         it "is expected to new_status to be waiting_validation" do
@@ -51,11 +47,11 @@ RSpec.feature "Admin::Suggestions::WaitingValidations", type: :feature do
 
         it "is expected to render in development option" do
           visit suggestions_index_admin_path(kind: :in_progress)
-          expect(find_all(class: "change-to-development").size).to match 1
+          expect(find_all(class: "change-to-development").size).to match 0
         end
 
         it "is expected to be redirect to in progress suggestions" do
-          expect(page).to have_current_path suggestions_index_admin_path
+          expect(page).to have_current_path suggestions_index_admin_path(kind: :in_progress)
         end
 
       end
